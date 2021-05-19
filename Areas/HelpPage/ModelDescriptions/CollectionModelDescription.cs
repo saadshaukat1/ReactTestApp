b8ca3208ecdfc7b70e776640dd2d2b71a61f172d
saadshaukat1 <m.saadshaukat@gmail.com>
2021-05-19 23:29:01 +0500

    Add project files.

---
+++ Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ReactTestApp.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReactTestApp.WebApiApplication" Language="C#" %>
